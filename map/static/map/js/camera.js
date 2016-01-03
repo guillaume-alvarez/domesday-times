766e855c4b5dbc78dd66714d2aa4e269a48a0f34
galvarez <guillaume.alvarez@gmail.com>
2016-01-03 12:35:57 +0100

    User can easily scroll the map with drag'n drop. Zoom is quite simple though.

--- map/static/map/js/camera.js
+++ map/static/map/js/camera.js
@@ -6,7 +6,7 @@ var Camera = function() {
     this.y = Number.MAX_SAFE_INTEGER;
     this.width = 0;
     this.height = 0;
-    this.scale = 1.0
+    this.scale = 1.0;
 };
 
 Camera.prototype.include = function(x, y) {
@@ -18,15 +18,30 @@ Camera.prototype.include = function(x, y) {
 
 var BORDER = 20;
 
-Camera.prototype.init = function(width, height) {
-    scaleX = (width - BORDER * 2) / this.width;
-    scaleY = (height - BORDER * 2) / this.height;
+Camera.prototype.init = function(renderer, viewport, stage) {
+    this.renderer = renderer;
+    this.viewport = viewport;
+    this.stage = stage;
+
+    // compute scale so that all the scene is in the screen
+    scaleX = (this.renderer.width - BORDER * 2) / this.width;
+    scaleY = (this.renderer.height - BORDER * 2) / this.height;
     this.scale = Math.min(scaleX, scaleY);
+
+    this.apply();
 };
 
-Camera.prototype.apply = function(viewport) {
-    viewport.scale.x = this.scale;
-    viewport.scale.y = this.scale;
-    viewport.position.x = -this.x * this.scale + BORDER;
-    viewport.position.y = -this.y * this.scale + BORDER;
+Camera.prototype.zoom = function(delta) {
+    this.scale += this.scale * delta;
+    this.apply();
+    this.renderer.render(stage);
+};
+
+Camera.prototype.apply = function() {
+    this.viewport.scale.x = this.scale;
+    this.viewport.scale.y = this.scale;
+    this.viewport.position.x = -this.x * this.scale + BORDER;
+    this.viewport.position.y = -this.y * this.scale + BORDER;
+
+    this.renderer.render(stage);
 };
--- map/static/map/js/pixiview.js
+++ map/static/map/js/pixiview.js
@@ -8,31 +8,93 @@ renderer.autoResize = true;
 // 10px for scroll bars and such
 renderer.resize(window.innerWidth - 10, window.innerHeight - 10);
 
+var stage = new PIXI.Container();
+stage.interactive = true;
+var world = new PIXI.ParticleContainer();
+var camera = new Camera();
+
 //Add the canvas to the HTML document
 document.body.appendChild(renderer.view);
+// IE9+, Chrome, Safari, Opera
+document.addEventListener("mousewheel", mouseWheelHandler, false);
+// Firefox
+document.addEventListener("DOMMouseScroll", mouseWheelHandler, false);
+
 
 PIXI.loader
     .add("villageImage", villageImage)
     .add("placesJson", placesJson)
+    .load(setup);
 
-    .load(function (loader, resources) {
-        var container = new PIXI.ParticleContainer();
-        var texture = resources.villageImage.texture;
-        var camera = new Camera();
+function setup(loader, resources) {
+    var texture = resources.villageImage.texture;
 
-        for (var i in resources.placesJson.data) {
-            var sprite = new PIXI.Sprite(texture);
-            var place = resources.placesJson.data[i];
-            sprite.position.x = place.fields.longitude * texture.width * 100.0;
-            // latitude is from south, Y is from screen top
-            sprite.position.y = -place.fields.latitude * texture.height * 100.0;
+    for (var i in resources.placesJson.data) {
+        var sprite = new PIXI.Sprite(texture);
+        var place = resources.placesJson.data[i];
+        sprite.position.x = place.fields.longitude * texture.width * 100.0;
+        // latitude is from south, Y is from screen top
+        sprite.position.y = -place.fields.latitude * texture.height * 100.0;
 
-            camera.include(sprite.position.x, sprite.position.y);
-            container.addChild(sprite);
-        }
+        camera.include(sprite.position.x, sprite.position.y);
+        world.addChild(sprite);
+    }
+
+    // required to be able to click on it
+    var graphics = new PIXI.Graphics();
+    graphics.beginFill(0xFFFFFF, 0);
+    graphics.lineStyle(0);
+    graphics.drawRect(0, 0, renderer.width, renderer.height);
+    graphics.endFill();
+    stage.addChildAt(graphics, 0);
+
+    stage.addChild(world);
+
+    // setup events
+    stage
+        // events for drag start
+        .on('mousedown', onDragStart)
+        .on('touchstart', onDragStart)
+        // events for drag end
+        .on('mouseup', onDragEnd)
+        .on('mouseupoutside', onDragEnd)
+        .on('touchend', onDragEnd)
+        .on('touchendoutside', onDragEnd)
+        // events for drag move
+        .on('mousemove', onDragMove)
+        .on('touchmove', onDragMove);
 
 
-        camera.init(renderer.width, renderer.height);
-        camera.apply(container);
-        renderer.render(container);
-    });
+    camera.init(renderer, world, stage);
+}
+
+function mouseWheelHandler(e) {
+    // reverse Firefox’s detail value
+    var delta = e.wheelDelta || -e.detail;
+    camera.zoom(0.1 * delta);
+}
+
+function onDragStart(event) {
+    // store a reference to the data
+    // the reason for this is because of multitouch
+    // we want to track the movement of this particular touch
+    world.dragStart = event.data.getLocalPosition(world.parent);
+    world.dragStart.x -= world.position.x;
+    world.dragStart.y -= world.position.y;
+    world.dragging = true;
+}
+
+function onDragEnd() {
+    world.dragging = false;
+    // set the interaction data to null
+    world.data = null;
+}
+
+function onDragMove(event) {
+    if (world.dragging) {
+        var newPosition = event.data.getLocalPosition(world.parent);
+        world.position.x = newPosition.x - world.dragStart.x;
+        world.position.y = newPosition.y - world.dragStart.y;
+        renderer.render(stage);
+    }
+}
