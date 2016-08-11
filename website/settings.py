d6a474b9c085e8d6ecee04cfac0a36fe56b6fd95
galvarez <guillaume.alvarez@gmail.com>
2016-08-11 13:44:09 +0200

    Compress heavy data sent to the GUI, like the lsit of places.

--- website/settings.py
+++ website/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE_CLASSES = [
+    'django.middleware.gzip.GZipMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
